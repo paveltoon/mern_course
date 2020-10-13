358b4bba4c759ba3810853cf9e0f137befd18a1a
Павел Леонидович Копылов <KopylovPL@mosreg.ru>
2020-10-13 13:15:04 +0300

    Changed config

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use('/t', require('./routes/redirect.routes'))
 
 if (process.env.NODE_ENV === "production") {
   app.use('/', express.static(path.join(__dirname, 'client', 'build')))
-  console.log(process.env.NODE_ENV)
+  
   app.get('*', (req, res) => {
     return res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   })
